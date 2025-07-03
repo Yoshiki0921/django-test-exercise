2ccb6bfc0615320c88e3062982036ef7595b973e
1f10240177 <s1f102401775@iniad.org>
2025-07-03 16:43:42 +0900

    コード修正5

--- todo/tests.py
+++ todo/tests.py
@@ -59,8 +59,8 @@ class TaskViewTestCase(TestCase):
 
     def test_index_post(self):
         client = Client()
-        data={'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
-        response= client.post('/', data)
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        response = client.post('/', data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
